2e867dc47d61464250234d4a3278f0856ac7c169
treastrain / Tanaka Ryoga <tr@tret.jp>
2020-08-22 16:37:51 +0900

    Initial Commit

---
+++ DepthPhotoViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DepthPhotoViewer
+//
+//  Created by treastrain on 2020/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
